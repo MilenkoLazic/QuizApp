12de4f2dfb99043bab33b4d13fe1b8fc0ecf96ad
unknown <miks.lazic@gmail.com>
2025-11-13 21:00:04 +0100

    initial commit

---
+++ QuizApp/Data/Questions.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizApp.Data
+{
+    public class Questions
+    {
+        public int id;
+        public string question;
+        public string description;
+        public List<String> answers;
+
+        public String multiple_correct_answers;
+        public List<string> correct_answers;
+        public string explanation;
+        public string category;
+        public string difficulty;
+
+    }
+}
